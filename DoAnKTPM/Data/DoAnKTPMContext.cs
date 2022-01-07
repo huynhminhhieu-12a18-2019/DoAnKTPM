148540cac75486551429db9e1186eb5e8790348f
Minh-Hieu2021 <89077535+Minh-Hieu2021@users.noreply.github.com>
2022-01-07 14:30:03 +0700

    Initial commit

---
+++ DoAnKTPM/Data/DoAnKTPMContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DoAnKTPM.Models;
+
+namespace DoAnKTPM.Data
+{
+    public class DoAnKTPMContext : DbContext
+    {
+        public DoAnKTPMContext (DbContextOptions<DoAnKTPMContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DoAnKTPM.Models.Account> Accounts { get; set; }
+
+        public DbSet<DoAnKTPM.Models.Cart> Carts { get; set; }
+
+        public DbSet<DoAnKTPM.Models.Invoice> Invoices { get; set; }
+
+        public DbSet<DoAnKTPM.Models.InvoiceDetail> InvoiceDetails { get; set; }
+
+        public DbSet<DoAnKTPM.Models.Product> Products { get; set; }
+
+        public DbSet<DoAnKTPM.Models.ProductType> ProductTypes { get; set; }
+    }
+}
---
+++ DoAnKTPM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DoAnKTPM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
