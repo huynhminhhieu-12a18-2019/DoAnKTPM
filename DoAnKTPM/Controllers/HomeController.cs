1c2e3c1edbc3cf4493078cbe3f1c7f9fbb1d05f1
Minh-Hieu2021 <89077535+Minh-Hieu2021@users.noreply.github.com>
2022-02-21 23:09:01 +0700

    Thêm trang pay và cart và xử lí mua hàng

--- DoAnKTPM/Controllers/HomeController.cs
+++ DoAnKTPM/Controllers/HomeController.cs
@@ -106,7 +106,153 @@ namespace DoAnKTPM.Controllers
         {
             return View();
         }
+        public async Task<IActionResult> Cart()
+        {
+            string username = HttpContext.Session.GetString("AccountUsername");
+            var carts = _context.Carts.Include(c => c.Account).Include(c => c.Product)
+                                      .Where(c => c.Account.Username == username);
+            ViewBag.Total = carts.Sum(c => c.Quantity * c.Product.Price);
+            return View(await carts.ToListAsync());
+        }
+        public IActionResult Addcart(int id)
+        {
+            return Addcart(id, 1);
+        }
+        [HttpPost]
+        public IActionResult Addcart(int productId, int quantity)
+        {
+            if (HttpContext.Session.GetString("AccountUsername") != null)
+            {
+                string username = HttpContext.Session.GetString("AccountUsername");
+                int accountId = _context.Accounts.FirstOrDefault(a => a.Username == username).Id;
+                Cart cart = _context.Carts.FirstOrDefault(c => c.AccountId == accountId && c.ProductId == productId);
+                if (cart == null)
+                {
+                    cart = new Cart();
+                    cart.AccountId = accountId;
+                    cart.ProductId = productId;
+                    cart.Quantity = quantity;
+                    _context.Carts.Add(cart);
+                }
+                else
+                {
+                    cart.Quantity += quantity;
+                }
+                _context.SaveChanges();
+                return RedirectToAction("Cart");
+            }
+            else
+            {
+                return RedirectToAction("Login", "Home");
+            }
 
+        }
+        /// xóa item trong cart
+        [Route("/RemoveCart/{productid:int}", Name = "RemoveCart")]
+        public IActionResult RemoveCart([FromRoute] int productid)
+        {
+            Cart cart = _context.Carts.FirstOrDefault();
+            var cartitem = _context.Carts.Where(p => p.Product.Id == productid);
+            foreach (Cart c in cartitem)
+            {
+                _context.Carts.Remove(c);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Cart");
+        }
+
+        [HttpPost]
+        [Route("/UpdateCart/{productid:int}", Name = "UpdateCart")]
+        public IActionResult UpdateCart(int productid, int quantity)
+        {
+            Cart cart = _context.Carts.FirstOrDefault();
+            var cartitem = _context.Carts.Where(p => p.Product.Id == productid);
+            foreach (Cart c in cartitem)
+            {
+                c.Quantity = quantity;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Cart");
+        }
+        public IActionResult RemoveAllCart()
+        {
+            Cart cart = _context.Carts.FirstOrDefault();
+            var cartitem = _context.Carts;
+            foreach (Cart c in cartitem)
+            {
+                _context.Carts.Remove(c);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Cart");
+        }
+        public IActionResult Pay()
+        {
+            string username = HttpContext.Session.GetString("AccountUsername");
+            ViewBag.Account = _context.Accounts.Where(a => a.Username == username).FirstOrDefault();
+            ViewBag.CartsTotal = _context.Carts.Include(c => c.Product).Include(c => c.Account)
+                                                .Where(c => c.Account.Username == username)
+                                                .Sum(c => c.Quantity * c.Product.Price);
+            return View("Pay");
+        }
+        [HttpPost]
+        public IActionResult Pay([Bind("ShippingAddress,ShippingPhone")] Invoice invoice)
+        {
+            string username = HttpContext.Session.GetString("AccountUsername");
+            if (!CheckStock(username))
+            {
+                ViewBag.ErrorMessage = "Có sản phẩm đã hết hàng. Vui lòng kiểm tra lại";
+                ViewBag.Account = _context.Accounts.Where(a => a.Username == username).FirstOrDefault();
+                ViewBag.CartsTotal = _context.Carts.Include(c => c.Product).Include(c => c.Account)
+                                                    .Where(c => c.Account.Username == username)
+                                                    .Sum(c => c.Quantity * c.Product.Price);
+                return View("Pay");
+            }
+            //Thêm hoá đơn
+            DateTime now = DateTime.Now;
+            invoice.Code = now.ToString("yyMMddhhmmss");
+            invoice.AccountId = _context.Accounts.FirstOrDefault(a => a.Username == username).Id;
+            invoice.IssuedDate = now;
+            invoice.Total = _context.Carts.Include(c => c.Product).Include(c => c.Account)
+                                                    .Where(c => c.Account.Username == username)
+                                                    .Sum(c => c.Quantity * c.Product.Price);
+            _context.Add(invoice);
+            _context.SaveChanges();
+            //Thêm chi tiết hoá đơn
+            List<Cart> carts = _context.Carts.Include(c => c.Product).Include(c => c.Account)
+                                                    .Where(c => c.Account.Username == username).ToList();
+            foreach (Cart c in carts)
+            {
+                InvoiceDetail invoiceDetail = new InvoiceDetail();
+                invoiceDetail.InvoiceId = invoice.Id;
+                invoiceDetail.ProductId = c.ProductId;
+                invoiceDetail.Quantity = c.Quantity;
+                invoiceDetail.UnitPrice = c.Product.Price;
+                _context.Add(invoiceDetail);
+            }
+            _context.SaveChanges();
+            //Trừ số lượng tồn kho và xoá giỏ hàng
+            foreach (Cart c in carts)
+            {
+                c.Product.Stock -= c.Quantity;
+                _context.Carts.Remove(c);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
+        }
+        private bool CheckStock(string username)
+        {
+            List<Cart> carts = _context.Carts.Include(c => c.Product).Include(c => c.Account)
+                                                    .Where(c => c.Account.Username == username).ToList();
+            foreach (Cart c in carts)
+            {
+                if (c.Product.Stock < c.Quantity)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         // POST: Accounts/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -119,7 +265,7 @@ namespace DoAnKTPM.Controllers
             {
                 _context.Add(account);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Login","Home");
+                return RedirectToAction("Login", "Home");
             }
             return View(account);
         }
